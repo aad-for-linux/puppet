fbc4ca79a998f1eeb96570c907b498331c2f8b04
Ada-Codina <89468347+Ada-Codina@users.noreply.github.com>
2021-10-28 11:16:22 -0600

    removed class apt section
    and updated libpam-aad to pam-aad

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -3,28 +3,11 @@ node default {
     'Ubuntu': {
       case $::lsbdistcodename {
         'bionic': {
-          class { 'apt':
-            update => {
-              frequency => 'daily',
-              loglevel  => 'debug'
-            }
-          }
-          apt::key { 'Launchpad PPA for Lucas Ramage':
-            id     => 'C7277B00CF5A8F6CF14D9A36A2C0BCEBD63BA8C1',
-            server => 'keyserver.ubuntu.com'
-          }
-          apt::key { 'Launchpad PPA for lramage@cyberninjas.com':
-            id     => '8C7C0551ED21508C4A8CC83368D35504E523B6B5',
-            server => 'keyserver.ubuntu.com'
-          }
-          apt::ppa { 'ppa:lramage/sds': }
-          apt::ppa { 'ppa:jnchi/ppa': }
-
           package { 'libnss-aad':
             ensure => installed
           }
 
-          package { 'libpam-aad':
+          package { 'pam-aad':
             ensure => installed
           }
 
